3686468f974e544b952a185c27d0ad5416cc253e
Julian Poma <jpoma@duck.com>
2025-07-26 16:49:47 -0300

    Add "New flight" button on page header

--- resources/js/components/app-sidebar-header.tsx
+++ resources/js/components/app-sidebar-header.tsx
@@ -1,7 +1,9 @@
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { type BreadcrumbItem as BreadcrumbItemType } from '@/types';
+import { Plus } from 'lucide-react';
 import { ReactNode } from 'react';
+import { Button } from './ui/button';
 
 type Props = {
   breadcrumbs?: BreadcrumbItemType[];
@@ -10,13 +12,16 @@ type Props = {
 
 export function AppSidebarHeader({ breadcrumbs = [], children }: Props) {
   return (
-    <header className="flex h-10 shrink-0 items-center gap-2 border-b border-sidebar-border/50 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4">
+    <header className="border-sidebar-border/50 flex h-10 shrink-0 items-center justify-between gap-2 border-b px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4">
       <div className="flex items-center gap-2">
         <SidebarTrigger className="-ml-1" />
         <Breadcrumbs breadcrumbs={breadcrumbs} />
       </div>
 
-      {children}
+      <Button>
+        <Plus />
+        New flight
+      </Button>
     </header>
   );
 }
--- resources/js/components/ui/button.tsx
+++ resources/js/components/ui/button.tsx
@@ -1,32 +1,29 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from 'react';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-1 whitespace-nowrap rounded text-sm font-medium transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[1px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
-        default:
-          "text-primary-foreground shadow-xs hover:bg-primary-hover border-[0.5px] border-button-border",
-        secondary:
-          "text-secondary-foreground hover:text-secondary-hover-foreground",
-        ghost:
-          "text-muted hover:text-fg-primary"
+        default: 'text-primary-foreground shadow-xs hover:bg-hover-primary border-[0.5px] border-button-border',
+        secondary: 'text-secondary-foreground hover:text-secondary-hover-foreground',
+        ghost: 'text-muted hover:text-fg-primary',
       },
       size: {
-        default: "h-6 px-2",
-        icon: "size-6 flex-none"
+        default: 'h-6 px-2',
+        icon: 'size-6 flex-none',
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      variant: 'default',
+      size: 'default',
     },
-  }
-)
+  },
+);
 
 function Button({
   className,
@@ -34,19 +31,13 @@ function Button({
   size,
   asChild = false,
   ...props
-}: React.ComponentProps<"button"> &
+}: React.ComponentProps<'button'> &
   VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
+    asChild?: boolean;
   }) {
-  const Comp = asChild ? Slot : "button"
+  const Comp = asChild ? Slot : 'button';
 
-  return (
-    <Comp
-      data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
-      {...props}
-    />
-  )
+  return <Comp data-slot="button" className={cn(buttonVariants({ variant, size, className }))} {...props} />;
 }
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
