916b6b6ef0f1994b678962cb6d01c1e6aec4e5fd
Julian Poma <jpoma@duck.com>
2025-08-03 10:00:50 -0300

    Create aircraft controller files

---
+++ app/Http/Controllers/AircraftController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAircraft;
+use App\Http\Requests\UpdateAircraft;
+use App\Models\Aircraft;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class AircraftController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $aircrafts = Aircraft::query()
+            ->where('user_id', Auth::id())
+            ->orderBy('ident', 'asc')
+            ->get();
+
+        return Inertia::render('aircrafts/page', [
+            'aircrafts' => $aircrafts,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAircraft $request)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAircraft $request, Aircraft $aircraft)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Aircraft $aircraft)
+    {
+        //
+    }
+}
